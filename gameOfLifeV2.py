615ecf86ea8e32c50d1e9ad15cddf20f33fec9e5
Dahakarful <tsar4le4grand@gmail.com>
2018-06-05 23:14:02 +0200

    Add options

--- gameOfLifeV2.py
+++ gameOfLifeV2.py
@@ -1,21 +1,24 @@
+import sys, getopt
+import random
 from tkinter import *
 
+master = Tk()
+w = None
+sizeSquare = 2
+generation = [0] * 800
 rule254 = {
-    "[1,1,1]" : 1, 
-    "[1,1,0]" : 1, 
-    "[1,0,1]" : 1, 
-    "[1,0,0]" : 1,
-    "[0,1,1]" : 1,
-    "[0,1,0]" : 1,
-    "[0,0,1]" : 1,
-    "[0,0,0]" : 0
+        "[1,1,1]" : 1, 
+        "[1,1,0]" : 1, 
+        "[1,0,1]" : 1, 
+        "[1,0,0]" : 1,
+        "[0,1,1]" : 1,
+        "[0,1,0]" : 1,
+        "[0,0,1]" : 1,
+        "[0,0,0]" : 0
 }
-generation = [[1,0,0,0,1,0,0,0,0,0,0,0]]
 
 def evolve():
-    last = len(generation) - 1
-    size = len(generation[last])
-    line = generation[last]
+    size = len(generation)
     linePlus = []
     for i in range(size):
         for k, v in rule254.items():
@@ -26,21 +29,58 @@ def evolve():
     generation.append(linePlus)
     return generation
 
-master = Tk()
-w = Canvas(master, width=500, height=500)
-w.pack()
+def gameOfLife(width, height, rule):
+    global w
+    w = Canvas(master, width=width, height=height)
+    w.grid()
+    # generateRandomLine(width, height)
+    w.pack()
+    mainloop()
 
-w.create_rectangle(250,0,258,10, fill="black")
+def generateRandomLine(width, height):
+    x1 = random.randrange(width)
+    y1 = 0
+    x2 = x1 + random.randrange(width)
+    y2 = 2
+    w.create_rectangle(x1, y1, x2, y2, fill="blue", outline='black')
 
-last = len(generation) - 1
-line = generation[last]
-print(line)
-marginTop = 0
-marginRight = 0
-# for l in line:
-#     if(l == 1):
-#         w.create_rectangle(marginRight, marginTop, 10, 10, fill="black")
-#     else:
-#         w.create_rectangle(marginRight, marginTop, 10, 10, fill="white")
-#     marginRight = marginRight + 10
-mainloop()
+def main(argv):
+    width = ''
+    height = ''
+    rule = ''
+    try:
+        opts, args = getopt.getopt(argv,"Hw:h:r:",["width=","height=","rule="])
+    except getopt.GetoptError:
+        print('Bad parameters ! Run command with -H option for help')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-H':
+            print('Choose the width and the height of the table with -w and -h\n')
+            print('And choose the rule to generate with -r\n')
+            print('The final command should be: gameOfLife.py -w <width> -h <height> -r <number rule>\n')
+            sys.exit()
+        elif opt in ("-w", "--width"):
+            try:
+                width = int(arg)
+            except ValueError:
+                print('-w: Option width need to be an int')
+                return
+        elif opt in ("-h", "--height"):
+            try:
+                height = int(arg)
+            except ValueError:
+                print('-w: Option height need to be an int')
+                return
+        elif opt in ("-r", "--rule"):
+            try:
+                rule = int(arg)
+            except ValueError:
+                print('-w: Option rule need to be an int')
+                return
+    if width != '' and height != '' and rule != '':
+        gameOfLife(width, height, rule)
+    else:
+        print('Run command with -H option for help')
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
